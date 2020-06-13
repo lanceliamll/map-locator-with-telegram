c20b86e50bbe588abd8e5442869239422ec92010
lanceliamll <lancelliotqqq@gmail.com>
2020-06-14 00:26:50 +0800

    Added modals for age, municipality, gender

--- client/screens/Profile.js
+++ client/screens/Profile.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext } from "react";
-import { Text, Button, View, Alert, StyleSheet } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { Text, Button, View, Alert, StyleSheet, Modal, TextInput } from "react-native";
 import { GlobalContext } from "../store/context/GlobalContext";
 import { Icon } from "react-native-elements"
 
@@ -13,7 +13,15 @@ const Profile = ({ navigation }) => {
       headerLeft: () => <Icon name='menu' onPress={() => navigation.openDrawer()} />,
       headerRight: () => <Icon name='exit-to-app' onPress={logoutToApp} />,
     })
-  });
+  }, []);
+
+  //modal
+  const [showAge, setShowAge] = useState(false);
+  const [showGender, setShowGender] = useState(false);
+  const [showMunicipality, setShowMunicipality] = useState(false);
+
+  // age
+  const [userAge, setUserAge] = useState(auth.age || null)
 
   const logoutToApp = () => {
     Alert.alert("Logout", "Are you sure you want to logout?",
@@ -25,7 +33,7 @@ const Profile = ({ navigation }) => {
         },
         { text: "OK", onPress: logout }
       ],
-    ) 
+    )
   }
 
   const { username, email, age, gender, municipality } = auth.user
@@ -45,18 +53,78 @@ const Profile = ({ navigation }) => {
       <View style={styles.textTitleContainer}>
         <Text style={styles.textTitle}>Age:</Text>
         <Text style={styles.textLabel}>{age === null ? "Not specified" : age}</Text>
-        <Icon name='edit' />
+        <Icon name='edit' onPress={() => showAge ? setShowAge(false) : setShowAge(true)} />
       </View>
       <View style={styles.textTitleContainer}>
         <Text style={styles.textTitle}>Gender:</Text>
         <Text style={styles.textLabel}>{gender === null ? "Not specified" : gender}</Text>
-        <Icon name='edit' />
+        <Icon name='edit' onPress={() => showGender ? setShowGender(false) : setShowGender(true)} />
       </View>
       <View style={styles.textTitleContainer}>
         <Text style={styles.textTitle}>Municipality:</Text>
         <Text style={styles.textLabel}>{municipality === null ? "Not specified" : municipality}</Text>
-        <Icon name='edit' />
+        <Icon name='edit' onPress={() => showMunicipality ? setShowMunicipality(false) : setShowMunicipality(true)} />
       </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showAge}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.modalView}>
+          <TextInput
+            onChangeText={(text) => setUserAge(text)}
+            style={styles.input}
+            placeholder="Age"
+            keyboardType="numeric"
+            maxLength={3}
+          />
+          <View>
+            <Button title="Save" />
+            <Text></Text>
+            <Button onPress={() => setShowAge(false)} title="Cancel" />
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showGender}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.modalView}>
+          <TextInput style={styles.input} placeholder="Gender" />
+          <View>
+            <Button title="Save" />
+            <Text></Text>
+            <Button onPress={() => setShowGender(false)} title="Cancel" />
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showMunicipality}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.modalView}>
+          <TextInput style={styles.input} placeholder="Municipality" />
+          <View>
+            <Button title="Save" />
+            <Text></Text>
+            <Button onPress={() => setShowMunicipality(false)} title="Cancel" />
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
@@ -91,13 +159,41 @@ const styles = StyleSheet.create({
     paddingBottom: 10
   },
   textTitle: {
-    fontSize: 15,
+    fontSize: 12,
     width: 160,
   },
   textLabel: {
-    fontSize: 15,
+    fontSize: 13,
     width: 160,
     fontWeight: "bold"
-  }
+  },
+  modalView: {
+    marginTop: 150,
+    height: 200,
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 15,
+    backgroundColor: "white",
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  cancelButton: {
+    marginTop: 10
+  },
+  input: {
+    marginTop: 15,
+    marginBottom: 15,
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
 })
 
